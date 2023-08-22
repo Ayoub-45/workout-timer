254db54c638ff91c2f3a62a33f7bc67a5758429d
unknown <ayoubnasraoui161@yahoo.fr>
2023-08-22 20:09:03 +0200

    Use helper functions in Effects

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import clickSound from './ClickSound.m4a';
 import { memo } from 'react';
 
@@ -8,20 +8,26 @@ function Calculator({ workouts, allowSound }) {
   const [speed, setSpeed] = useState(90);
   const [durationBreak, setDurationBreak] = useState(5);
   const [duration,setDuration]=useState(0)
+  
+  useEffect(function(){
+    const playSound = function () {
+      if (!allowSound) return;
+      const sound = new Audio(clickSound);
+      sound.play();
+    };
+    playSound()
+  },[duration,allowSound])
   useEffect(function(){
     setDuration((number * sets * speed) / 60 + (sets - 1) * durationBreak)
+  
   },[number,sets,speed,durationBreak])
   //const duration = (number * sets * speed) / 60 + (sets - 1) * durationBreak;
   const mins = Math.floor(duration);
   const seconds = (duration - mins) * 60;
 
-  const playSound = function () {
-    if (!allowSound) return;
-    const sound = new Audio(clickSound);
-    sound.play();
-  };
 function handleInc(){
   setDuration(duration=>Math.floor(duration)+1);
+
 }
 function handleDec(){
   setDuration(duration=>duration>1?Math.ceil(duration)-1:0)
